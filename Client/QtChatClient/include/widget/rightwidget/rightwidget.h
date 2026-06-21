3ff53398a36bbe1cb5d87dd62809beedd9788568
龚柏荣 <gbr@edu.nuc.email>
2026-06-21 16:07:34 +0800

    完成右侧聊天窗口

--- Client/QtChatClient/include/widget/rightwidget/rightwidget.h
+++ Client/QtChatClient/include/widget/rightwidget/rightwidget.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <public.h>
+#include <utils/log.h>
 namespace ChatWidget
 {
     class RightWidget : public QWidget
@@ -7,10 +8,14 @@ namespace ChatWidget
         Q_OBJECT
        private:
         explicit RightWidget(QWidget *parent = nullptr);
-        // void _InitRightWidget();         // 初始化右侧信息区
+        void _InitRightWidget();         // 初始化右侧信息区
         static RightWidget *s_instance;  // 单例实例指针
        public:
         ~RightWidget() override;
         static RightWidget *GetInstance(QWidget *parent = nullptr);  // 获取单例实例的静态方法
+
+        QWidget *m_titleBar;         // 上方标题栏
+        QLabel *m_titleLabel;        // 标题标签
+        QPushButton *m_titleButton;  // 标题按钮
     };
 }  // namespace ChatWidget
--- Client/QtChatClient/src/widget/rightwidget/rightwidget.cpp
+++ Client/QtChatClient/src/widget/rightwidget/rightwidget.cpp
@@ -1,5 +1,21 @@
 #include <widget/rightwidget/rightwidget.h>
 using namespace ChatWidget;
+using namespace Log;
+
+namespace
+{
+    constexpr auto kRightWidgetStyle = "QWidget#rightWidget { background-color: #ecf0f1; }";
+
+    constexpr auto kTitleBarStyle = "QWidget#titleBar { background-color: #bdc3c7; }";
+
+    constexpr auto kTitleBarLabelStyle = "QLabel { font-size: 18px; font-weight: bold; color: #2c3e50; }";
+
+    constexpr auto kTitleBarButtonIconPath = ":/images/more.png";
+    constexpr auto kTitleBarButtonStyle =
+        "QPushButton { background-color: transparent; border: none; border-radius: 4px; }"
+        "QPushButton:hover { background-color: #ecf0f1; border: none; }"
+        "QPushButton:pressed { background-color: #bdc3c7; border: none; }";
+}  // namespace
 
 RightWidget *RightWidget::s_instance = nullptr;  // 初始化单例实例指针
 
@@ -11,11 +27,58 @@ RightWidget *RightWidget::GetInstance(QWidget *parent)
 
 RightWidget::RightWidget(QWidget *parent) : QWidget(parent)
 {
+    // 初始化资源
+    this->m_titleBar = new QWidget(this);
+    this->m_titleLabel = new QLabel(this->m_titleBar);
+    this->m_titleButton = new QPushButton(this->m_titleBar);
+
+    // 初始化UI界面
+    this->_InitRightWidget();
+}
+
+RightWidget::~RightWidget() = default;
+
+void RightWidget::_InitRightWidget()
+{
+    if (this->m_titleBar == nullptr || this->m_titleLabel == nullptr || this->m_titleButton == nullptr)
+    {
+        LogInfo(LogLevel::ERROR, "右侧信息区资源初始化失败");
+        exit(-1);
+    }
+
     this->setObjectName("rightWidget");
     this->setAttribute(Qt::WA_StyledBackground, true);
     this->setMinimumWidth(350);  // 设置右侧信息区最小高度,宽度,根据窗口大小自动调整
     this->setMinimumHeight(350);
-    this->setStyleSheet("QWidget#rightWidget { background-color: #ecf0f1; }");
-}
+    // 背景
+    this->setStyleSheet(kRightWidgetStyle);
+    // 布局
+    QVBoxLayout *rightLayout = new QVBoxLayout(this);
+    rightLayout->setContentsMargins(0, 0, 0, 0);
+    rightLayout->setSpacing(0);
+    this->setLayout(rightLayout);
 
-RightWidget::~RightWidget() = default;
+    // 上方标题栏
+    this->m_titleBar->setFixedHeight(50);
+    this->m_titleBar->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    this->m_titleBar->setObjectName("titleBar");
+    this->m_titleBar->setAttribute(Qt::WA_StyledBackground, true);
+    this->m_titleBar->setStyleSheet(kTitleBarStyle);
+    rightLayout->addWidget(this->m_titleBar);
+    // 标签有一个label和一个按钮
+    QHBoxLayout *titleBarLayout = new QHBoxLayout(this->m_titleBar);
+    titleBarLayout->setContentsMargins(0, 0, 0, 0);
+    titleBarLayout->setSpacing(0);
+    // - 标签
+    this->m_titleLabel->setStyleSheet(kTitleBarLabelStyle);
+#if DEBUG_CODE
+    this->m_titleLabel->setText("聊天信息");
+#endif
+    titleBarLayout->addWidget(this->m_titleLabel);
+    // - 按钮
+    this->m_titleButton->setFixedSize(30, 30);
+    this->m_titleButton->setIconSize(QSize(30, 30));
+    this->m_titleButton->setIcon(QIcon(kTitleBarButtonIconPath));
+    this->m_titleButton->setStyleSheet(kTitleBarButtonStyle);
+    titleBarLayout->addWidget(this->m_titleButton);
+}
