0090ba3a8d98ddaa34792683e3cc391ebcbfe38f
龚柏荣 <gbr@edu.nuc.email>
2026-08-04 17:15:59 +0800

    补充窗口属性

--- Client/QtChatClient/include/widget/user_info_widget/UserInfoWidget.h
+++ Client/QtChatClient/include/widget/user_info_widget/UserInfoWidget.h
@@ -20,7 +20,15 @@ namespace ChatWidget
         Message m_message;  // 消息对象,用于显示消息
 
         QPointer<QPushButton> m_avatarButton;  // 聊天对象的头像
-        QPointer<QLabel> m_userNameTag;  // 聊天对象的用户名
+        QPointer<QLabel> m_userNameTag;        // 名字
+        QPointer<QLabel> m_userName;           // 用户名字
+        QPointer<QLabel> m_userTag;            // 昵称
+        QPointer<QLabel> m_userTagName;        // 用户昵称
+        QPointer<QLabel> m_userDescription;    // 用户签名
+        QPointer<QLabel> m_userPhone;          // 用户手机号
 
+        QPointer<QPushButton> m_applyfriendButton;  // 申请好友按钮
+        QPointer<QPushButton> m_sendMessageButton;  // 发送消息按钮
+        QPointer<QPushButton> m_delFriendButton;    // 删除好友按钮
     };
 }  // namespace ChatWidget
